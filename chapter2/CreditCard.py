3dfaa43fbc8172272a8e28220b4d3be53f3042bb
raghavgr94 <raghavgr94@gmail.com>
2017-11-09 21:25:33 +0300

    :credit_card: Implemented credit card class

---
+++ chapter2/CreditCard.py
@@ -0,0 +1,54 @@
+class CreditCard:
+    """A consumer credit card"""
+
+    def __init__(self, customer, bank, account, limit):
+        """Create a new credit card instance.
+
+        The initial balance is zero.
+
+        :param customer: the name of customer
+        :param bank:     the name of bank
+        :param account:     the account identifier
+        :param limit:    credit limit(in $)
+        """
+
+        self._customer = customer
+        self._bank = bank
+        self._account = account
+        self._limit = limit
+        self._balance = 0
+
+    def get_customer(self):
+        """Return name of the customer."""
+        return self._customer
+
+    def get_bank(self):
+        """Return name of bank."""
+        return self._bank
+
+    def get_account(self):
+        """Return the card identifying number (a string)."""
+        return self._account
+
+    def get_limit(self):
+        """Return current credit limit."""
+        return self._limit
+
+    def get_balance(self):
+        """Return current balance."""
+        return self._balance
+
+    def charge(self, price):
+        """Charge given price to the card, assuming sufficient credit limit.
+
+        Return True if charge was processed. False if charge was denied.
+        """
+        if price + self._balance > self._limit:     # if charge exceeds limit
+            return False                            # deny charge
+        else:
+            self._balance += price
+            return True
+
+    def make_payment(self, amount):
+        """Process customer payment that reduces balance."""
+        self._balance -= amount
