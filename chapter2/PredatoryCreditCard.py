93cce6b78195ee92475ae03e6f398479c497a6a0
raghavgr94 <raghavgr94@gmail.com>
2017-11-12 13:28:45 +0300

    :sparkles: Practiced inheritance with PredatoryCreditCard

---
+++ chapter2/PredatoryCreditCard.py
@@ -0,0 +1,42 @@
+from chapter2 import CreditCard
+
+
+class PredatoryCreditCard(CreditCard.CreditCard):
+    """An extension to CreditCard that compounds interest adn fees"""
+
+    def __init__(self, customer, bank, account, limit, apr):
+        """
+        Create a new PredatoryCreditCard instance.
+
+        Initial balance is zero.
+
+        :param customer:the name of customer
+        :param bank:    the name of bank
+        :param account: the account identifier
+        :param limit:   credit limit(in $)
+        :param apr:     annual percentage rate (e.g., 0.0825 or 8.25% APR)
+        """
+
+        super().__init__(customer, bank, account, limit)
+        self._apr = apr
+
+    def charge(self, price):
+        """Charge given price to the card, assuming sufficient card limit.
+
+        Return True if charge was processed.
+        Return False and add $5 fee if charge is denied.
+        """
+
+        success = super().charge        # call inherited method
+        if not success:
+            self._balance += 5          # add fee
+        return success                  # return True or False
+
+    def process_month(self):
+        """Assess monthly interest on outstanding balance."""
+        if self._balance > 0:
+            # if balance is positive, convert APR to monthly multiplicative factor
+            monthly_factor = pow(1 + self._apr, 1/12)
+            self._balance *= monthly_factor
+
+
