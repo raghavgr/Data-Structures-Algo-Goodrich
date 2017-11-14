02ef495fb854fd2daf6e779bc51b5f2d08e5c63f
raghavgr94 <raghavgr94@gmail.com>
2017-11-14 21:30:57 +0300

    :scroll: Worked on exercises 2.28, 2.30

--- chapter2/PredatoryCreditCard.py
+++ chapter2/PredatoryCreditCard.py
@@ -4,21 +4,23 @@ from chapter2 import CreditCard
 class PredatoryCreditCard(CreditCard.CreditCard):
     """An extension to CreditCard that compounds interest adn fees"""
 
-    def __init__(self, customer, bank, account, limit, apr):
+    def __init__(self, customer, bank, account, limit, apr, charges):
         """
         Create a new PredatoryCreditCard instance.
 
         Initial balance is zero.
 
-        :param customer:the name of customer
-        :param bank:    the name of bank
-        :param account: the account identifier
-        :param limit:   credit limit(in $)
-        :param apr:     annual percentage rate (e.g., 0.0825 or 8.25% APR)
+        :param customer:    the name of customer
+        :param bank:        the name of bank
+        :param account:     the account identifier
+        :param limit:       credit limit(in $)
+        :param apr:         annual percentage rate (e.g., 0.0825 or 8.25% APR)
+        :param charges:     track the number of times charges was called
         """
 
         super().__init__(customer, bank, account, limit)
         self._apr = apr
+        self._charges = charges             # 2.28
 
     def charge(self, price):
         """Charge given price to the card, assuming sufficient card limit.
@@ -26,11 +28,14 @@ class PredatoryCreditCard(CreditCard.CreditCard):
         Return True if charge was processed.
         Return False and add $5 fee if charge is denied.
         """
+        self._charges += 1
         print(price,"is being charged")
-        success = super().charge(price)        # call inherited method
+        success = super().charge(price)     # call inherited method
         if not success:
-            self._balance += 5          # add fee
-        return success                  # return True or False
+            self._balance += 5              # add fee
+        if self._charges > 10:
+            self._balance -= 1
+        return success                      # return True or False
 
     def process_month(self):
         """Assess monthly interest on outstanding balance."""
