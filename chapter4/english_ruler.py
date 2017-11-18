4fd529abdd77ce92e502c6218d506a534bdec04f
raghavgr94 <raghavgr94@gmail.com>
2017-11-18 22:04:53 +0300

    :straight_ruler: English Ruler recursive code

---
+++ chapter4/english_ruler.py
@@ -0,0 +1,21 @@
+def draw_line(tick_length, tick_label=''):
+    """ Draw one line with given tick length along with optional label"""
+    line = '-' * tick_length
+    if tick_label:
+        line += ' ' + tick_label
+    print(line)
+
+def draw_interval(center_length):
+    """ Draw tick interval based on the central tick length"""
+    if center_length > 0:
+        draw_interval(center_length - 1)
+        draw_line(center_length)
+        draw_interval(center_length - 1)
+
+def draw_ruler(num_inches, major_length):
+    draw_line(major_length, '0')
+    for i in range(1, 1 + num_inches):
+        draw_interval(major_length - 1)
+        draw_line(major_length, str(i))
+
+draw_ruler(3, 5)
