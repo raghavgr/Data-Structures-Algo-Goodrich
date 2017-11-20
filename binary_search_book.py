36d3601fcfcca6e234ef99555853dea0d30d6f91
raghavgr94 <raghavgr94@gmail.com>
2017-11-20 20:07:41 +0300

    :mag_right: Book's binary search implementation added

---
+++ binary_search_book.py
@@ -0,0 +1,20 @@
+def binary_search(data, target, low, high):
+    """
+    Book's implementation of binary search. 
+    Return True if target is found in indicated portion of a Python list.
+    The search only considers the portion from data[low] to data[high] inclusive. 
+
+    """
+
+    if low  > high:                 # indicates that interval is empty, mo match found
+        return False
+    else:
+        mid = (low + high) // 2
+        if target == data[mid]:
+            return True             # found a match             
+        elif target < data[mid]:
+            # recur on portion left of the middle
+            return binary_search(data, target, low, mid - 1)
+        else:
+            # recur on portion right of the middle
+            return binary_search(data, target, mid + 1, high)
