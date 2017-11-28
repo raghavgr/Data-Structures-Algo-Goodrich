d9aedd23f6195cd4214e1c29e9f40126e6bf1998
raghavgr94 <raghavgr94@gmail.com>
2017-11-28 20:48:12 +0300

    played around with unique

--- chapter3/unique.py
+++ chapter3/unique.py
@@ -1,7 +1,10 @@
-def unique1(ls):
-    for i in range(len(ls)):
-        for j in range(i + 1, len(ls)):
-            if ls[i] == ls[j]:
+""" Three implementations of find unique element in an array """
+
+def unique1(arr):
+    """ brute force implementation """
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] == arr[j]:
                 return False
 
     return True
@@ -9,9 +12,9 @@ def unique1(ls):
 print(unique1([1, 2, 1, 3]))
 
 
-def unique2(ls):
+def unique2(arr):
     """ Better unique implementation """
-    temp = sorted(ls)
+    temp = sorted(arr)
     for i in range(1, len(temp)):
         if temp[i - 1] == temp[i]:
             return False
